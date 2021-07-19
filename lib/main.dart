277201a22b1df441601396c89915bde9c02cff6d
Design n Techie <deepaanandhan001@gmail.com>
2021-07-19 18:18:24 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lottie Animation',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lottie Animation'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Lottie.asset('asset/devs-app.json'),
+          Lottie.asset('asset/sending-email.json')
+        ]),
+      ),
+    );
+  }
+}
